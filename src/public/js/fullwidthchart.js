ae80ed624992985ee491c597ff9be3093e93ef7f
ZX\Administrator <zzzxh158@gmail.com>
2016-12-18 18:12:37 +0800

    Button1，加了图和表

---
+++ src/public/js/fullwidthchart.js
@@ -0,0 +1,70 @@
+$(function () {
+  Highcharts.chart('container111', {
+    chart: {
+      zoomType: 'xy'
+    },
+    title: {
+      text: '产品销售趋势表'
+    },
+    xAxis: [{
+      categories: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      crosshair: true
+    }],
+    yAxis: [{ // Primary yAxis
+      labels: {
+        format: '{value}°C',
+        style: {
+          color: Highcharts.getOptions().colors[1]
+        }
+      },
+      title: {
+        text: '气温',
+        style: {
+          color: Highcharts.getOptions().colors[1]
+        }
+      }
+    }, { // Secondary yAxis
+      title: {
+        text: '销量',
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
+      },
+      labels: {
+        style: {
+          color: Highcharts.getOptions().colors[0]
+        }
+      },
+      opposite: true
+    }],
+    tooltip: {
+      shared: true
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      x: 120,
+      verticalAlign: 'top',
+      y: 100,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+    },
+    series: [{
+      name: '销量',
+      type: 'column',
+      yAxis: 1,
+      data: [649.9, 71.5, 106.4, 129.2, 1144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+      tooltip: {
+        valueSuffix: ' 件'
+      }
+
+    }, {
+      name: '气温',
+      type: 'spline',
+      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+      tooltip: {
+        valueSuffix: '°C'
+      }
+    }]
+  });
+});
