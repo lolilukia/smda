fa4af3a8a8ed02db989339adcc2495f071798bd5
luoxiaodan <xxd375454026@sina.com>
2016-12-22 13:29:59 +0800

    增加销售趋势数据绑定

--- src/public/js/fullwidthchart.js
+++ src/public/js/fullwidthchart.js
@@ -1,4 +1,4 @@
-$(function () {
+function drawfullchart() {
   Highcharts.chart('container111', {
     chart: {
       zoomType: 'xy'
@@ -7,7 +7,7 @@ $(function () {
       text: '产品销售趋势表'
     },
     xAxis: [{
-      categories: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      categories:catetend,
       crosshair: true
     }],
     yAxis: [{ // Primary yAxis
@@ -54,18 +54,18 @@ $(function () {
       name: '销量',
       type: 'column',
       yAxis: 1,
-      data: [49.9, 71.5, 106.4, 129.2, 176.0, 226.0, 285.6, 248.5, 266.4, 194.1, 95.6, 54.4],
+      data: month_sales,
       tooltip: {
-        valueSuffix: ' 件'
+        valueSuffix: '元'
       }
 
     }, {
       name: '气温',
       type: 'spline',
-      data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+      data: temperaturetend,
       tooltip: {
         valueSuffix: '°C'
       }
     }]
   });
-});
+};
